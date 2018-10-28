b29933628f340b47ac4d7bd47fdcce22dc38e993
mrose2611 <33336494+mrose2611@users.noreply.github.com>
2018-10-28 12:32:01 -0700

    Completed sphere.js

--- assignment-files-sphere/math/sphere.js
+++ assignment-files-sphere/math/sphere.js
@@ -30,15 +30,51 @@ var Sphere = function(origin = new Vector3(), radius = 1) {
 		//              normal: 'a vector3 containing a unit length normal at the intersection',
 		//              distance: 'a scalar containing the intersection distance from the ray origin'
 		//          }
+		d = ray.direction.clone();
+		o = ray.origin.clone();
+		a = d.dot(d);
+		b1 = o.subtract(this.origin);
+		b2 = d.multiplyScalar(2);
+		b = b2.dot(b1);
+		c = b1.dot(b1) - this.radius * this.radius;
 
+		discriminant = (b * b) - (4 * a * c);
+		
+		if(discriminant < 0){ //discriminant is negative so the ray just doesn't hit
+			var result = {
+				hit:false,
+				point:null,
+			};
+		}
+		else{
+			alphaPos = (-1 * b + Math.sqrt(discriminant))/(2 * a);
+			alphaNeg = (-1 * b - Math.sqrt(discriminant))/(2 * a);
+			closer = Math.min(alphaPos, alphaNeg);
+			if(closer < 0){ //negative values so the ray does not hit or is inside the sphere
+				var result = {
+					hit: false,
+					point: null
+				};
+			}
+			else { //the ray will hit the sphere
+				oVector = ray.origin.clone();
+				dVector = ray.direction.clone();
+				intersect = oVector.add(dVector.multiplyScalar(closer)); //pRay = origin + alpha(direction vector)
+				var result = {
+					hit: true,
+					point: intersect,
+					normal: intersect.normalized(),
+					distance: closer
+				};
+			}
 		// todo - fill this in with the correct values
-		var result = {
-			hit: null,
-			point: null,
-			normal: null,
-			distance: null
-		};
-
+			/*var result = {
+				hit: null,
+				point: null,
+				normal: null,
+				distance: null
+			};*/
+		}
 		return result;
 	}
 };
